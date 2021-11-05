5e8d8f537b667979d870606b902a232665d8566e
YuYang-Dev <79308509+YuYang-Dev@users.noreply.github.com>
2021-11-05 22:30:02 +0800

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,13 @@
+// potato
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyBy4xfcDVg1nV09MFd8HYRArL1hl92APSc",
+    authDomain: "notifire-4c482.firebaseapp.com",
+    projectId: "notifire-4c482",
+    storageBucket: "notifire-4c482.appspot.com",
+    messagingSenderId: "585151939307",
+    appId: "1:585151939307:web:7422dc50f3e20dd460f4ae"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
